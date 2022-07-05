78e421e29dc8426b483f6cb5cda3111c77ffdf9c
Martins Greizis <grei0003@hz.nl>
2022-07-06 00:46:09 +0200

    test .gitignore push

--- resources/views/file.blade.php
+++ resources/views/file.blade.php
@@ -45,10 +45,7 @@
                             </div>
                         </div>
                         <div class="mt-3 text-center pb-3">
-                            <button type="submit"
-                                class="w-full h-12 text-lg w-32 bg-blue-600 rounded text-white hover:bg-blue-700">
-                                Save
-                            </button>
+                            <button type="submit" class="button">Save</button>
                         </div>
                         <button class="button is-primary">Alright</button>
                     </form>
