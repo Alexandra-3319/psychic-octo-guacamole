6c2c4622dc299d11c003fdad9215347778951246
garc0039 <garc0039@hz.nl>
2022-07-06 00:54:29 +0200

    fresh start

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\FileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('upload', [FileController::class, 'index'])->name('file');
-Route::post('upload', [FileController::class, 'store'])->name('file.store');
