9df8bbb2c6dd86d5623e8af8b1e1c5ae4c25473e
garc0039 <garc0039@hz.nl>
2022-07-06 00:33:35 +0200

    design init

---
+++ resources/views/main/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<hmtl>
+    <head>
+        <meta charset="utf-8">
+        <title>
+            Motivation Page
+        </title>
+    </head>
+    <body>
+        <div>
+
+        </div>
+    </body>
+</hmtl>
